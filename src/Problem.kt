3f057c94cfe0a45bb455ca24d26b7094e4dfc754
Florian Knop <florian_knop@hotmail.com>
2017-10-24 19:08:38 +0200

    initial fromFile

--- src/Problem.kt
+++ src/Problem.kt
@@ -1,7 +1,36 @@
-class Problem {
-    companion object {
-        fun fromFile() {
+import java.io.File
+import java.util.*
 
+data class Edge(val u: Int, val v: Int, val weight: Int, val resource: Int)
+data class Node(val x: Double, val y: Double)
+
+data class Problem(val nodes: List<Node>, val edges: List<Edge>, val source: Int, val dest: Int, val capacity: Int) {
+
+    companion object {
+        fun fromFile (path: String): Problem {
+            val file = File(path)
+            val scanner = Scanner(file)
+            val n = scanner.nextInt()
+            val m = scanner.nextInt()
+            val s = scanner.nextInt()
+            val t = scanner.nextInt()
+            val capacity = scanner.nextInt()
+
+            val nodes = 0.rangeTo(n).map { i ->
+                val x = scanner.nextDouble()
+                val y = scanner.nextDouble()
+                Node(x, y)
+            }
+
+            val edges = 0.rangeTo(m).map { i ->
+                val u = scanner.nextInt()
+                val v = scanner.nextInt()
+                val w = scanner.nextInt()
+                val r = scanner.nextInt()
+                Edge(u, v, w, r)
+            }
+
+            return Problem(nodes, edges, s, t, capacity)
         }
     }
 }
